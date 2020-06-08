ae20661c09cdbd61c36568b716f2737f341a7b0d
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-06-07 21:19:18 -0300

    src/*

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import data from "./data/form01.json";
+
+// console.log(data.fields);
+
+// console.log(data.fields.map(function(i) {
+//   console.log(i);
+// }));
+
+
+class Controls extends Component {
+  renderField(field) {
+    // console.log(field);
+    return (
+      <Form.Group controlId="exampleForm.SelectCustom">
+        <Form.Label>{field.name}</Form.Label>
+        <Form.Control as="select" custom>
+          <option>option 1</option>
+          <option>option 2</option>
+          <option>option 3</option>
+          <option>option 4</option>
+        </Form.Control>
+      </Form.Group>
+    )
+  }
+  render() {
+    // if (data.fields.length === 0) {
+    //   return <p>Nenhum item</p>
+    // }
+    return data.fields.map(this.renderField)
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form>
+        <Controls />
+      </Form>
+      <Button onClick={() => console.log('oi')}>Salvar</Button>
     </div>
   );
 }
