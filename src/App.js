c3be5ba0d763de8198ec9afb4e1857315ada724b
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-06-12 16:41:34 -0300

    add formik

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
 import React, { Component } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import data from "./data/form01.json";
-
-// console.log(data.fields);
-
-// console.log(data.fields.map(function(i) {
-//   console.log(i);
-// }));
+// import { Button, Form } from 'react-bootstrap';
 
 
-class Controls extends Component {
-  renderField(field) {
-    // console.log(field);
-    return (
-      <Form.Group controlId="exampleForm.SelectCustom">
-        <Form.Label>{field.name}</Form.Label>
-        <Form.Control as="select" custom>
-          <option>option 1</option>
-          <option>option 2</option>
-          <option>option 3</option>
-          <option>option 4</option>
-        </Form.Control>
-      </Form.Group>
-    )
-  }
-  render() {
-    // if (data.fields.length === 0) {
-    //   return <p>Nenhum item</p>
-    // }
-    return data.fields.map(this.renderField)
-  }
-}
+//
+// Escolha o exemplo
+//
+import FormBasic from './Step1';
 
 
 function App() {
   return (
     <div className="App">
-      <Form>
-        <Controls />
-      </Form>
-      <Button onClick={() => console.log('oi')}>Salvar</Button>
+      <FormBasic />
     </div>
   );
 }
