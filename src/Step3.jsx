26a8127c1f9aa5b000b8ba7c056156c97730b79f
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-06-14 12:35:16 -0300

    step3

--- src/Step3.jsx
+++ src/Step3.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import data from "./data/form01.json";
+
+//
+//
+//
+function generatorAllFields(arrFields) {
+
+  let generatorEachField = (field) => {
+    return (
+      <>
+        <label htmlFor="{field.id}">{field.name}</label>
+        <Field name="{field.id}" type="text" />
+        <ErrorMessage name="{field.id}" />
+      </>
+    )
+  }
+  
+  return arrFields.map(generatorEachField);
+}
 
 //
 //
@@ -35,22 +54,12 @@ const onSubmit = (values, { setSubmitting }) => {
 //
 //
 //
-const FormStep1 = () => {
+const FormStep3 = () => {
   return (
     <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit} >
       <Form>
 
-        <label htmlFor="firstName">First Name</label>
-        <Field name="firstName" type="text" />
-        <ErrorMessage name="firstName" />
-
-        <label htmlFor="lastName">Last Name</label>
-        <Field name="lastName" type="text" />
-        <ErrorMessage name="lastName" />
-
-        <label htmlFor="email">Email Address</label>
-        <Field name="email" type="email" />
-        <ErrorMessage name="email" />
+        {generatorAllFields(data.fields)}
 
         <button type="submit">Submit</button>
       </Form>
@@ -58,4 +67,4 @@ const FormStep1 = () => {
   );
 };
 
-export default FormStep1;
+export default FormStep3;
