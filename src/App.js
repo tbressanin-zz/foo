fb8d676545e2206b68be0ade0fbda7ac8a2abc1b
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-06-19 11:30:24 -0300

    step5 formik + bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 // import React, { Component } from 'react';
-// import { Button, Form } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //
 // Escolha o exemplo
 //
-import FormBasic from './Step4';
+import FormBasic from './Step5';
 
 function App() {
   return (
-    <div className="App">
-      <FormBasic />
-    </div>
+    <Container fluid="md">
+      <Row>
+        <Col>
+          <FormBasic />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/Step5.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Formik, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { Form, Button } from 'react-bootstrap';
+import data from "./data/form01.json";
+
+//
+// Gerar os campos do formulário
+//
+function generatorAllFields(arrFields) {
+
+  // para cada campo, um formato diferente
+  let generatorEachField = (field) => {
+    switch (field.type) {
+      case "short_text":
+        return (
+          <Form.Group controlId="exampleForm.{field.id}">
+            <Form.Label>{field.name}</Form.Label>
+            <Form.Control type="text" name="{field.id}" placeholder="Enter anything" />            
+            <ErrorMessage name="{field.id}" />
+          </Form.Group>
+        )
+      case "single_select":
+        return (
+          <Form.Group controlId="exampleForm.{field.id}">
+            <Form.Label>{field.name}</Form.Label>
+            <Form.Control type="text" name="{field.id}" placeholder="Enter anything" />            
+            <ErrorMessage name="{field.id}" />
+          </Form.Group>
+        )
+      case "dynamic_select":
+        return (
+          <Form.Group controlId="exampleForm.{field.id}">
+            <Form.Label>{field.name}</Form.Label>
+            <Form.Control as="select" custom>
+              <SelectOptions data={field.options} />
+            </Form.Control>
+            <ErrorMessage name="{field.id}" />
+          </Form.Group>
+        )
+      default:
+        return (<>field not found !</>)
+    }
+  }
+
+  // laço nos campos do formulário
+  return arrFields.map(generatorEachField);
+}
+
+//
+// para gerar as `options` dos selects
+//
+class SelectOptions extends React.Component {
+  getOption(field) {
+    // console.log(field);
+    return <option value={field.key}>{field.label}</option>
+  }
+  render() {
+    return this.props.data.map(this.getOption);
+  }
+}
+
+//
+const initialValues = { firstName: '', lastName: '', email: '' };
+
+//
+const validationSchema = Yup.object({
+  firstName: Yup.string()
+    .max(15, 'Must be 15 characters or less')
+    .required('Required'),
+  lastName: Yup.string()
+    .max(20, 'Must be 20 characters or less')
+    .required('Required'),
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('Required'),
+})
+
+//
+const onSubmit = (values, { setSubmitting }) => {
+  setTimeout(() => {
+    alert(JSON.stringify(values, null, 2));
+    setSubmitting(false);
+  }, 400);
+}
+
+//
+const FormStep4 = () => {
+  return (
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit} >
+      <Form>
+        {generatorAllFields(data.fields)}
+        <Button variant="primary" type="submit">Submit</Button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default FormStep4;
